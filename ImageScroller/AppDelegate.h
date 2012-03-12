a38e0944fad2a7317005c209b9d6e710101ceddc
Anurag Solanki <anurag@vinsol.com>
2012-03-12 10:17:21 +0530

    Initial Commit

---
+++ ImageScroller/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ImageScroller
+//
+//  Created by Anurag Solanki on 12/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ImageScroller/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImageScroller
+//
+//  Created by Anurag Solanki on 12/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
